fab57561b4257c02a8a29e3646d8f66a7f91ce9e
Rohan Mondal <mondalrohan108@gmail.com>
2022-03-08 23:24:46 +0530

    updated data

--- src/components/data.js
+++ src/components/data.js
@@ -3,20 +3,20 @@ const data = [
   { rating: 4.1, category: "women", size: "s" },
   { rating: 3.8, category: "women", size: "m" },
   { rating: 3.9, category: "men", size: "xl" },
-  { rating: 3.5, category: "men", size: "l" },
+  { rating: 2.5, category: "men", size: "l" },
   { rating: 3.7, category: "kids", size: "xxl" },
   { rating: 4.3, category: "kids", size: "s" },
   { rating: 4.7, category: "women", size: "xxl" },
-  { rating: 4.8, category: "women", size: "m" },
+  { rating: 2.8, category: "women", size: "m" },
   { rating: 4.5, category: "kids", size: "xl" },
   { rating: 3.1, category: "men", size: "l" },
   { rating: 2.5, category: "women", size: "xl" },
   { rating: 4.2, category: "men", size: "m" },
   { rating: 4.1, category: "kids", size: "s" },
   { rating: 3.9, category: "kids", size: "xxl" },
-  { rating: 3.7, category: "women", size: "l" },
+  { rating: 2.1, category: "women", size: "l" },
   { rating: 3.5, category: "men", size: "m" },
-  { rating: 4.5, category: "kids", size: "s" }
+  { rating: 2.5, category: "kids", size: "s" }
 ];
 const GetAdditionalData = () => data;
 export default GetAdditionalData;
